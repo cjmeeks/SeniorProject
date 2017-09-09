05967812266ece5b85d4e1fa11da2649133f3b62
Clifford Meeks <cliffmeeks2214@gmail.com>
2017-09-09 17:56:53 -0500

    initial commit

---
+++ code-generator/Main.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+
+module Main where
+import Data.Proxy (Proxy (Proxy))
+import Elm (Spec (Spec), specsToDir, toElmTypeSource, toElmDecoderSource, toElmEncoderSource)
+import Servant.Elm (ElmOptions (..), defElmImports, defElmOptions, generateElmForAPIWith, UrlPrefix (Static))
+import Api.Types
+import Data.Text as DT
+import Api.Example.Types (Dice(..))
+
+elmOpts :: ElmOptions
+elmOpts =
+  defElmOptions
+    { urlPrefix = Static "/servant-elm-template" }
+
+elmImports :: [Text]
+elmImports = [ "import Dict exposing (Dict)"
+              ]
+
+specs :: [Spec]
+specs =
+  [ 
+    Spec ["Shared", "Generated"]
+         ( defElmImports `append` (DT.intercalate "\n" elmImports)
+         : toElmTypeSource (Proxy :: Proxy Dice)
+         : toElmDecoderSource (Proxy :: Proxy Dice)
+         : toElmEncoderSource (Proxy :: Proxy Dice)
+         : generateElmForAPIWith elmOpts  (Proxy :: Proxy Api)
+         )
+  ]
+
+main :: IO ()
+main = specsToDir specs "frontend/elm"
