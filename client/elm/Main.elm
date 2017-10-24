e05660cdd9dcf3b8f2e90b6ec168ab85ffdfd379
Clifford Meeks <cliffmeeks2214@gmail.com>
2017-10-24 14:13:33 -0500

    Revert "Revert "Addworkout (#3)""
    This reverts commit cdf02619f82481697b2c1ddf051ca6f24e9dc474.

--- client/elm/Main.elm
+++ client/elm/Main.elm
@@ -1,60 +1,80 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.Events exposing (..)
-import Html.Attributes exposing(..)
+import Html.Attributes exposing (..)
 import Random
 import Types exposing (Msg(..), Page(..), Model)
 import Rest exposing (rollDice)
 import Nav.View as NavView
+import AddWorkout.View as Add
 import Dashboard.View as Dash
 import Bootstrap.Navbar as Nav
 import Navigation exposing (Location)
 import Routing exposing (parseLocation)
 import Init exposing (initialModel, initUser)
 
+
 main : Program Never Model Msg
 main =
-  Navigation.program LocationChange
-    { init = initialModel
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Navigation.program LocationChange
+        { init = initialModel
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    NavMsg state ->
-      ( { model | navbar = state }, Cmd.none )
-    
-    HandleUser us ->
-      ( { model | user = us }, Cmd.none )
+    case msg of
+        NavMsg state ->
+            ( { model | navbar = state }, Cmd.none )
 
-    HandleError err ->
-      ( model, Cmd.none )
+        HandleUser us ->
+            ( { model | user = us }, Cmd.none )
 
-    LocationChange loc ->
-      ({model | currentPage = parseLocation loc}, Cmd.none)
-    
+        HandleError err ->
+            ( model, Cmd.none )
+
+        LocationChange loc ->
+            ( { model | currentPage = parseLocation loc }, Cmd.none )
+
+
 
 -- SUBSCRIPTIONS
+
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Nav.subscriptions model.navbar NavMsg
-  
+
+
 
 -- VIEW
+
+
 view : Model -> Html Msg
 view model =
-  let
-      curView = case model.currentPage of
-                  Dashboard -> Dash.view model
-                  AddWorkout -> div [ class "main-view" ] [text "addworkout"]
-                  Workouts -> div [ class "main-view" ] [text "workouts"]
-                  Stats -> div [ class "main-view" ] [text "stats"]
-                  Profile ->  div [ class "main-view" ] [text "profile"]
-  in     
-    div []
-      [ NavView.newView model
-      , curView
-      ]
+    let
+        curView =
+            case model.currentPage of
+                Dashboard ->
+                    Dash.view model
+
+                AddWorkout ->
+                    div [ class "main-row" ] [ Add.view model ]
+
+                Workouts ->
+                    div [ class "main-view" ] [ text "workouts" ]
+
+                Stats ->
+                    div [ class "main-view" ] [ text "stats" ]
+
+                Profile ->
+                    div [ class "main-view" ] [ text "profile" ]
+    in
+        div []
+            [ NavView.newView model
+            , curView
+            ]
